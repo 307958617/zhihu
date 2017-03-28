20bde1abab03822f1d4cabc131f5265a9256c10a
SmallFish <307958617@qq.com>
2017-03-28 17:40:59 +0800

    6第6步完成了

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreQuestionRequest;
 use App\Question;
+use App\Topic;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -42,13 +43,26 @@ class QuestionsController extends Controller
      */
     public function store(StoreQuestionRequest $request)
     {
+        $topics = $this->normalizeTopics($request->topics);//这里的topics就是select2传递过来的数组数据
         $date = [
             'title' => $request->get('title'),
             'body' => $request->get('body'),
             'user_id' => Auth::id()  //表示是谁发布的问题
         ];
         $question = Question::create($date);//保持到数据库中
+        $question->topics()->attach($topics);//将关联关系写入中间表
         return redirect(route('questions.show',[$question->id]));//跳转到问题显示页面，[]里面的内容是这个文章的ID
+    }
+
+    private function normalizeTopics(array $topics){
+        return collect($topics)->map(function ($topic){ //通过collect()和map()方法遍历数组
+            if (is_numeric($topic)){   //如果是数字就返回数字
+                Topic::find($topic)->increment('questions_count');//将话题关联的问题数量+1
+                return (int)$topic;
+            }
+            $newTopic = Topic::create(['name'=>$topic,'questions_count'=>1]);//如果不是数字就表示没有这个话题因此新建一个topic，将问题数设置为1
+            return $newTopic->id;//返回这个topic的id值
+        })->toArray();//转换成数组
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('api')->get('/topics', function (Request $request) {
+    $topics = \App\Topic::where('name','like','%'.$request->query('q').'%')
+                                               ->pluck('id','name');
+    return response()->json(['items'=>$topics]);
+});
